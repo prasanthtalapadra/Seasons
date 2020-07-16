f52dca9bdde3fc2a3f1b0ad5a0ffaab7f1007d6e
Prasanth Talapadra <prasanth.talapadra@gmail.com>
2020-07-16 15:04:23 +0530

    added error message

--- src/index.js
+++ src/index.js
@@ -3,21 +3,38 @@ import ReactDOM from 'react-dom';
 // import SeasonDisplay from './SeasonDisplay'
 
 
-class App extends React.Component{
-    render(){
-        
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => { console.log(position)},
-        (err) => { console.log(err)}
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null,errorMessage:'' }
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => { 
+                this.setState({lat:position.coords.latitude})
+            },
+            (err) => { 
+                this.setState({errorMessage:err.message});
+            }
+
+        );
+    }
+
+
+    render() {
+
     
-    );
-        return (
-            <div>
-                Latitude:
-            </div>
-        )
+        if(this.state.errorMessage && !this.state.lat){
+            return<div>Error:{this.state.errorMessage}</div>
+            }
+
+        if(!this.state.errorMessage && this.state.lat){
+                return<div>Latitude:{this.state.lat}</div>
+                } 
+        
+        return  <div>Loading!</div>
     }
 }
 
 
-ReactDOM.render(<App/>,document.querySelector('#root'))
+ReactDOM.render(<App />, document.querySelector('#root'))
